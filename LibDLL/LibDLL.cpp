8f533141676dc357b6613ca06fee9769b9d3c618
siittance <144900566+siittance@users.noreply.github.com>
2024-05-31 14:06:16 +0300

    fd

---
+++ LibDLL/LibDLL.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdbool.h>
+#include <string.h>
+
+extern "C" __declspec(dllexport) bool serchstr(const char* str, const char* chars) {
+
+    for (int i = 0; chars[i] != '\0'; i++) {
+        if (strchr(str, chars[i]) == NULL) {
+            return false;
+        }
+    }
+
+    return true;
+}
---
+++ plusbibl/plusbibl.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+#include <iostream>
+#pragma execution_character_set("utf-8")
+using namespace std;
+
+typedef bool(*CheckCharactersFunction)(const char*, const char*);
+
+int main() {
+    SetConsoleCP(65001); // устанавливаем кодировку входящего потока данных на UTF-8
+    SetConsoleOutputCP(65001); // устанавливаем кодировку исходящего потока данных на UTF-8
+
+    cout << "Введите строчку для поиска символов: ";
+    string str;
+    getline(cin, str);
+
+    cout << "Введите символы для поиска в строке: ";
+    string sim;
+    getline(cin, sim);
+
+    HINSTANCE hDLL = LoadLibrary(L"LibDLL.dll");
+    typedef bool (*serchstr)(const char*, const char*);
+
+    serchstr findCharsInString = (serchstr)GetProcAddress(hDLL, "serchstr");
+
+    bool res = findCharsInString(str.c_str(), sim.c_str());
+
+    if (res) {
+        cout << "Все нашло" << endl;
+    }
+    else {
+        cout << "Не все нашло" << endl;
+    }
+
+    FreeLibrary(hDLL);
+
+    return 0;
+}
+
